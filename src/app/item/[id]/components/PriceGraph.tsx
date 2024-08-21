36dab1973f27a8b9494182021bbf718bb0dd521c
MelnCat <melncatuwu@gmail.com>
2024-08-21 00:33:52 -0700

    oops

--- src/app/item/[id]/components/PriceGraph.tsx
+++ src/app/item/[id]/components/PriceGraph.tsx
@@ -4,12 +4,13 @@ import "chart.js/auto";
 import "chartjs-adapter-luxon";
 import { Line } from "react-chartjs-2";
 
-export const PriceGraph = ({ data }: { data: [Date, number][][] }) => (
-	<div className={styles.chartContainer}>
+export const PriceGraph = ({ data }: { data: [Date, number][][] }) => {
+	console.log(data[0].length, data[1].length);
+	return <div className={styles.chartContainer}>
 		<Line
 			data={{
 				datasets: [{ data: data[0].map(x => x[1]), label: "Lowest Price" }, { data: data[1].map(x => x[1]), label: "Listing Price" }],
-				labels: data.map(x => x[0]),
+				labels: data[0].map(x => x[0]),
 			}}
 			options={{
 				scales: {
@@ -30,4 +31,4 @@ export const PriceGraph = ({ data }: { data: [Date, number][][] }) => (
 			}}
 		/>
 	</div>
-);
+};
