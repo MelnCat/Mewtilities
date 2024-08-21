548478da415e13dfb18774014b3c778b4e7ea00b
MelnCat <melncatuwu@gmail.com>
2024-08-21 00:42:42 -0700

    fix

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -20,7 +20,7 @@ const createHistory = (entries: MarketEntry[]) => {
 		const price = lastPrice !== -1 && lowest > lastPrice * 2 ? lastPrice : lowest;
 		lastPrice = price;
 		times.push([new Date(time), price]);
-		const closest = dateSorted.find(x => +x.creationTime <= time);
+		const closest = dateSorted.findLast(x => +x.creationTime <= time);
 		realTimes.push([new Date(time), closest ? closest.priceCount / closest.itemCount : realTimes.at(-1)?.[1] ?? lastPrice]);
 	}
 	return [times, realTimes];
