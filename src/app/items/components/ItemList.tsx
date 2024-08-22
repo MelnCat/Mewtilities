9ce6ae9893ebc0eb894f1279ea08ff93ee149c5a
MelnCat <melncatuwu@gmail.com>
2024-08-22 01:25:01 -0700

    progress

--- src/app/items/components/ItemList.tsx
+++ src/app/items/components/ItemList.tsx
@@ -5,6 +5,11 @@ import { NoteValue, EssenceValue, CurrencyValue } from "@/components/currencyIco
 import { bestOffersByCurrency } from "@/util/util";
 import { Item, MarketEntry, ShopEntry, QuickSellEntry, Currency } from "@prisma/client";
 import useSWR from "swr";
+import { CSSProperties, useEffect, useMemo, useState } from "react";
+import NextAdapterApp from "next-query-params/app";
+import { QueryParamProvider, useQueryParam, StringParam, withDefault, NumberParam } from "use-query-params";
+import fuzzysort from "fuzzysort";
+import { useDebounce } from "use-debounce";
 
 const ItemBox = ({ item }: { item: Item & { marketEntries: (MarketEntry & { unitPrice: number })[]; shopEntries: ShopEntry[]; quickSellEntries: QuickSellEntry[] } }) => {
 	const market = item.marketEntries.toSorted((a, b) => a.unitPrice - b.unitPrice);
@@ -52,11 +57,56 @@ const ItemBox = ({ item }: { item: Item & { marketEntries: (MarketEntry & { unit
 };
 
 export const ItemList = () => {
-	const { data: items } = useSWR<Awaited<ReturnType<typeof getAllItems>>>("/api/items", async () => await (await fetch("/api/items", { cache: "force-cache", next: { revalidate: 300 } })).json());
+	const { data: items } = useSWR<Awaited<ReturnType<typeof getAllItems>>>(
+		"/api/items",
+		async () => await (await fetch("/api/items", { cache: "force-cache", next: { revalidate: 300 } })).json()
+	);
+	const [page, setPage] = useQueryParam("page", withDefault(NumberParam, 0));
+	const [name, setName] = useState(""); // useQueryParam("name", withDefault(StringParam, ""));
+	const [debouncedName] = useDebounce(name, 100);
+	const nameFilteredData = useMemo(() => {
+		if (!items) return null;
+		if (!debouncedName.trim()) return items;
+		const filtered = fuzzysort.go(debouncedName, items, { key: "name", threshold: 0.5 });
+		return filtered.map(x => x.obj);
+	}, [items, debouncedName]);
+	useEffect(() => {
+		setPage(0);
+	}, [nameFilteredData, setPage]);
 	return (
 		<>
 			<h1>Item Index</h1>
-			<article className={styles.itemList}>{items && items.map(x => <ItemBox item={x} key={x.id} />)}</article>
+			<section className={styles.search}>
+				<div className={styles.searchOption}>
+					<p>Name</p>
+					<input value={name} onChange={e => setName(e.target.value)} />
+				</div>
+				<div className={styles.searchOption}>
+					<button
+						disabled={nameFilteredData === null || !((page - 1) * 50 in nameFilteredData)}
+						onClick={nameFilteredData && (page - 1) * 50 in nameFilteredData && setPage(x => x - 1)}
+					>
+						{"<"}
+					</button>
+					<button
+						disabled={nameFilteredData === null || !((page + 1) * 50 in nameFilteredData)}
+						onClick={nameFilteredData && (page + 1) * 50 in nameFilteredData && setPage(x => x + 1)}
+					>
+						{">"}
+					</button>
+				</div>
+			</section>
+			<article className={styles.itemList}>
+				{nameFilteredData ? nameFilteredData.slice(page * 50, (page + 1) * 50).map(x => <ItemBox item={x} key={x.id} />) : "Loading..."}
+			</article>
 		</>
+	);
+};
+
+export const ItemDisplay = () => {
+	return (
+		<QueryParamProvider adapter={NextAdapterApp}>
+			<ItemList />
+		</QueryParamProvider>
 	);
 };
--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,15 +1,11 @@
-import { getAllItems } from "@/db/db";
+import { ItemDisplay } from "./components/ItemList";
 import styles from "./page.module.scss";
-import { Currency, Item, MarketEntry, QuickSellEntry, ShopEntry } from "@prisma/client";
-import { CurrencyValue, EssenceValue, NoteValue } from "@/components/currencyIcons";
-import { bestOffersByCurrency } from "@/util/util";
-import { ItemList } from "./components/ItemList";
 
 
 export default async function ItemsPage() {
 	return (
 		<main className={styles.main}>
-			<ItemList />
+			<ItemDisplay />
 		</main>
 	);
 }
