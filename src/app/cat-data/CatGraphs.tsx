6a4a9f4f44bf896073e15a40742b3ac335db044c
MelnCat <melncatuwu@gmail.com>
2024-08-30 16:56:05 -0700

    update

--- src/app/cat-data/CatGraphs.tsx
+++ src/app/cat-data/CatGraphs.tsx
@@ -2,33 +2,47 @@
 import { Cat } from "@prisma/client";
 import "chart.js/auto";
 import "chartjs-adapter-luxon";
-import { Line } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 import { groupBy } from "remeda";
 import styles from "./page.module.scss";
 import { useState } from "react";
 
-const OccurrenceGraph = ({ data, name }: { data: (number | null)[]; name: string }) => {
+const OccurrenceGraph = ({ data, name, percentage }: { data: (string | number | null)[]; name: string; percentage: boolean }) => {
 	const grouped = Object.entries(
 		groupBy(
 			data.filter(x => x !== null),
 			x => x
 		)
-	);
-	const min = Math.min(...grouped.map(x => +x[0]));
-	const max = Math.max(...grouped.map(x => +x[0]));
-	const values = data.length ? [...Array(max - min + 1)].map((x, i) => i + min).map(x => [x, grouped.find(y => +y[0] === x)?.[1]?.length ?? 0]) : [];
-	console.log(values);
+	).sort((a, b) => a[0].localeCompare(b[0]));
+	const Graph = !grouped.every(x => !isNaN(+x[0])) ? Bar : Line;
+	const values = data.length
+		? (() => {
+				if (grouped.every(x => !isNaN(+x[0]))) {
+					const min = Math.min(...grouped.map(x => +x[0]));
+					const max = Math.max(...grouped.map(x => +x[0]));
+					return [...Array(max - min + 1)].map((x, i) => i + min).map(x => [x, grouped.find(y => +y[0] === x)?.[1]?.length ?? 0]);
+				} else {
+					return grouped.map(x => [x[0], x[1].length]);
+				}
+		  })()
+		: [];
 	return (
 		<div className={styles.chartContainer}>
-			<Line
+			<Graph
 				data={{
-					datasets: [{ data: values.map(x => x[1]), label: "Count", fill: true }],
+					datasets: [{ data: values.map(x => x[1]).map(x => percentage ? +x / values.reduce((l, c) => l + +c[1], 0) : x), label: "Count", fill: true }],
 					labels: values.map(x => x[0]),
 				}}
 				options={{
 					scales: {
 						y: {
 							min: 0,
+							ticks: {
+								format: {
+									style: percentage ? "percent" : "decimal",
+									maximumFractionDigits: 10
+								},
+							},
 						},
 					},
 					plugins: {
@@ -44,9 +58,11 @@ const OccurrenceGraph = ({ data, name }: { data: (number | null)[]; name: string
 };
 
 export const CatGraphs = ({ data }: { data: Cat[] }) => {
+	(globalThis as any).cats = data;
 	const [origin, setOrigin] = useState("Any");
 	const origins = ["Any", ...new Set(data.map(x => x.origin))];
 	const [age, setAge] = useState("Any");
+	const [percentage, setPercentage] = useState(true);
 	const ages = ["Any", ...new Set(data.map(x => x.ageType))];
 	const filteredData = data.filter(x => origin === "Any" || x.origin === origin).filter(x => age === "Any" || x.ageType === age);
 	return (
@@ -71,16 +87,31 @@ export const CatGraphs = ({ data }: { data: Cat[] }) => {
 							</option>
 						))}
 					</select>
+				</div><div>
+					Percentages <input type="checkbox" checked={percentage} onChange={e => setPercentage(!percentage)} />
 				</div>
 			</section>
 			<section className={styles.graphRow}>
-				<OccurrenceGraph data={filteredData.map(x => x.strength)} name="Strength" />
-				<OccurrenceGraph data={filteredData.map(x => x.agility)} name="Agility" />
-				<OccurrenceGraph data={filteredData.map(x => x.health)} name="Health" />
-				<OccurrenceGraph data={filteredData.map(x => x.finesse)} name="Finesse" />
-				<OccurrenceGraph data={filteredData.map(x => x.perception)} name="Perception" />
-				<OccurrenceGraph data={filteredData.map(x => x.cleverness)} name="Cleverness" />
-				<OccurrenceGraph data={filteredData.map(x => x.luck)} name="Luck" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.strength)} name="Strength" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.agility)} name="Agility" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.health)} name="Health" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.finesse)} name="Finesse" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.perception)} name="Perception" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.cleverness)} name="Cleverness" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.luck)} name="Luck" />
+				<OccurrenceGraph
+					percentage={percentage}
+					data={filteredData.map(x => x.strength! + x.agility! + x.health! + x.finesse! + x.perception! + x.cleverness! + x.luck!).filter(x => !isNaN(x) && x !== 0)}
+					name="Stat Total"
+				/>
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.bravery)} name="Bravery" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.benevolence)} name="Benevolence" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.energy)} name="Energy" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.extroversion)} name="Extroversion" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.dedication)} name="Dedication" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.whiteMarks.match(/ ([a-zA-Z])\d+/)?.[1] ?? null)} name="White Type" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.whiteMarks.match(/ [a-zA-Z](\d+)/)?.[1] ?? 0)} name="White Number" />
+				<OccurrenceGraph percentage={percentage} data={filteredData.map(x => x.wind)} name="Wind" />
 			</section>
 		</>
 	);
